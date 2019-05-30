e900e080b31e156aa220c6bc9e13eb9d3bb19ee0
Oliver Sieweke <oliver.sieweke@protonmail.com>
2019-05-30 11:33:43 +0200

    Fix Port

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,7 +4,7 @@ const path = require("path")
 const express = require("express");
 const app = express();
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 
 app.use(express.static(path.join(__dirname, "../public")));
